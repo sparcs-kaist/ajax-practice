4561b80a995c394543221e467a0a98b6aa8c0883
Zeniuus <jsh56son@kaist.ac.kr>
2018-05-06 17:03:14 +0900

    Implement solution

--- static/js/index.js
+++ static/js/index.js
@@ -7,20 +7,41 @@ window.addEventListener('load', () => {
   const loginResultContainer = document.getElementById('login_result_container');
 
   idInput.addEventListener('focusout', (e) => {
-    alert('time to check id!'); /* Plz remove this line. */
     /*
      * TODO: 1. send ajax request to check if id is correct.
      * TODO: 2. if id is wrong, show red "Wrong ID!" text in wrongIdContainer.
      * TODO: 3. if correct, show green "Correct ID!" text in wrongIdContainer.
      */
+    axios.post('/api/check/id', {
+      id: idInput.value,
+    }).then((res) => {
+      if (res.data.match) {
+        wrongIdContainer.innerHTML = 'Correct ID!';
+        wrongIdContainer.style.color = 'green';
+      } else {
+        wrongIdContainer.innerHTML = 'Wrong ID!';
+        wrongIdContainer.style.color = 'red';
+      }
+    });
   });
 
   loginBtn.addEventListener('click', (e) => {
-    alert('button click!'); /* Plz remove this line. */
     /*
      * TODO: 1. send ajax request to check if login succeeds.
      * TODO: 2. if succeeds, show green "Login Success!" text in loginResultContainer.
      * TODO: 3. if not, show red "Login Failed..." text in loginResultContainer.
      */
+    axios.post('/api/login', {
+      id: idInput.value,
+      password: pwInput.value,
+    }).then((res) => {
+      if (res.data.success) {
+        loginResultContainer.innerHTML = 'Login Success!';
+        loginResultContainer.style.color = 'green';
+      } else {
+        loginResultContainer.innerHTML = 'Login Failed...';
+        loginResultContainer.style.color = 'red';
+      }
+    });
   });
 });
